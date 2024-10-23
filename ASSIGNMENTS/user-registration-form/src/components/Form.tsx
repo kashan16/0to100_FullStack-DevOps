7fd317298480d1043f8762d5e8ad0d7166c7486c
kashan <mohdkashan21165@gmail.com>
2024-10-23 19:43:40 +0530

    Did assignment on basic REACT

--- ASSIGNMENTS/user-registration-form/src/components/Form.tsx
+++ ASSIGNMENTS/user-registration-form/src/components/Form.tsx
@@ -135,7 +135,8 @@ const Form = () => {
         </div>
         <button
           type="submit"
-          className='w-full bg-indigo-600 text-white py-2 px-4 rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'>
+          disabled={!validate()}
+          className={`w-full ${!validate() ? 'bg-gray-400' : 'bg-indigo-600'} text-white py-2 px-4 rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`}>
           Register
         </button>
       </form>
